b3e89da567ba8bc03d1d76ddbefcf2be96093143
Apurba Adhikary <apurba.apurbaadhikary@gmail.com>
2023-05-27 07:40:42 +0000

    Api Fix

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
-const axios = require("axios");
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://your-api-url",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   timeout: 5000,
 });
 
